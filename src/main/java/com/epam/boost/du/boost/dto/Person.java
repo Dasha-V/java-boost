bf6ed4adee5874a7aee265da4e7a875f0ceb170d
Darya_Urubleuskaya <Darya_Urubleuskaya@epam.com>
2024-03-06 10:26:26 +0400

    implement PersonCountService, add unit test

---
+++ src/main/java/com/epam/boost/du/boost/dto/Person.java
@@ -0,0 +1,16 @@
+package com.epam.boost.du.boost.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Person
+{
+	private String id;
+	private String firstName;
+	private String lastName;
+}
